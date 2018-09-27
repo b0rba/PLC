ffab07b8c77ccb585cc3ea4bb0d8ae2477aef717
Matheus Luiz Borba Alves da Silva <mlbas@ug1c26.windows.cin.ufpe.br>
2018-09-27 16:26:48 -0300

    Aula 12, code

---
+++ aula12.hs
@@ -0,0 +1,36 @@
+
+
+type Nome = String
+data Pessoa = M Nome | F Nome
+
+instance Show Pessoa where
+    show (M n) = n
+    show (F n) = n
+
+instance Eq Pessoa where
+    (M n1) == (M n2) = n1 == n2
+    (F n1) == (F n2) = n1 == n2
+    _ == _ = False
+    
+    
+-- Avaliação (lazy) preguiçosa
+
+f :: Int -> Int -> Int
+f a b = a + 10
+
+g :: Int -> Int
+g x = x + g x 
+
+troca :: Integer -> a -> a -> a
+troca n x y
+  |n > 0 = x
+  |otherwise = y
+
+fk :: [Int] -> [Int] -> Int
+fk (x:xs) (y:ys) = x + y
+
+tailFat :: Int -> Int -> Int
+tailFat 0 x = x
+tailFat n x = tailFat (n-1) (n*x)
+
+fat n = tailFat n 1
