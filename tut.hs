8a83dc881d5e81616eb9cf8267448a446120a12f
Matheus Borba <mlbas@MacBook-Pro-de-Mario.local>
2018-10-07 18:45:35 -0300

    code from tutorial

---
+++ tut.hs
@@ -0,0 +1,21 @@
+import Data.List
+import System.IO
+
+getFirstLetter :: String -> String
+getFirstLetter [] = "Empty String"
+getFirstLetter all@(x:xs) = "The !(first letter) in " ++ all ++ " is " ++ xs
+
+areStringEq :: [Char] -> [Char] -> Bool
+areStringEq [] [] = True
+areStringEq (x:xs) (y:ys) = x == y && areStringEq xs ys
+areStringEq _ _ = False
+
+times4 :: Int -> Int
+times4 x = x * 4
+
+doMult :: (Int -> Int) -> Int
+doMult func = func 3
+
+num3Times4 = doMult times4
+
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
